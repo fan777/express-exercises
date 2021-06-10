53d823e3cd19013d9cf1950718a3648d94a80c04
fan777 <fan777@gmail.com>
2021-06-10 00:47:22 -0700

    completed exercise

---
+++ app.js
@@ -0,0 +1,96 @@
+const express = require('express');
+const ExpressError = require('./error');
+const { calcMean, calcMedian, calcMode } = require('./calc');
+
+const app = express();
+
+app.get('/mean', (req, res, next) => {
+  try {
+    let nums = convertParamsToNumArray(req.query.nums);
+    return res.send({
+      operation: 'mean',
+      value: calcMean(nums),
+    });
+  } catch (e) {
+    next(e);
+  }
+})
+
+app.get('/median', (req, res, next) => {
+  try {
+    let nums = convertParamsToNumArray(req.query.nums);
+    return res.send({
+      operation: 'median',
+      value: calcMedian(nums),
+    })
+  } catch (e) {
+    next(e);
+  }
+})
+
+app.get('/mode', (req, res, next) => {
+  try {
+    let nums = convertParamsToNumArray(req.query.nums);
+    return res.send({
+      operation: 'mode',
+      value: calcMode(nums),
+    })
+  } catch (e) {
+    next(e);
+  }
+})
+
+app.get('/all', (req, res, next) => {
+  try {
+    let nums = convertParamsToNumArray(req.query.nums);
+    return res.send({
+      operation: 'all',
+      mean: calcMean(nums),
+      median: calcMedian(nums),
+      mode: calcMode(nums),
+    })
+  } catch (e) {
+    next(e);
+  }
+})
+
+// 404 handler
+app.use((req, res, next) => {
+  const err = new ExpressError("Not Found", 404);
+  return next(err);
+});
+
+// general error handler
+app.use((err, req, res, next) => {
+  // the default status is 500 Internal Server Error
+  let status = err.status || 500;
+  let message = err.message;
+
+  // set the status and alert the user
+  return res.status(status).json({
+    error: { message, status }
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000")
+});
+
+/********************
+ * route input validation
+ */
+function convertParamsToNumArray(array) {
+  let nums = array.split(',');
+
+  if (!array)
+    throw new ExpressError('nums are required', 400)
+
+  if (nums.length < 2)
+    throw new ExpressError('more than one num expected', 400)
+
+  return nums.map(element => {
+    if (isNaN(element))
+      throw new ExpressError(`${element} is not a number`, 400)
+    return (Number(element));
+  })
+}
---
+++ calc.js
@@ -0,0 +1,57 @@
+function calcMean(nums) {
+  return nums.reduce((acc, current) => acc += current) / nums.length;
+}
+
+function calcMedian(nums) {
+  let midIdx = nums.length / 2;
+  nums.sort((a, b) => a - b);
+  if (nums.length % 2 == 0) {
+    return (nums[Math.floor(midIdx) - 1] + nums[Math.floor(midIdx)]) / 2;
+  } else {
+    return nums[Math.floor(midIdx)];
+  }
+}
+
+function calcMode(nums) {
+  let mode = [];
+
+  nums.sort((a, b) => a - b);
+
+  // get map of number of frequencies and convert to array
+  let frequency = Array.from(nums.reduce((acc, curr) => {
+    if (acc.has(curr))
+      acc.set(curr, acc.get(curr) + 1);
+    else
+      acc.set(curr, 1);
+    return acc;
+  }, new Map()), ([name, value]) => ([name, value]));
+
+  frequency.sort((a, b) => b[1] - a[1]);
+
+  for (let idx = 0; idx < frequency.length; idx++) {
+    if (idx == 0)
+      mode.push(frequency[idx][0]);
+    else if (idx > 0) {
+      if (frequency[idx][1] < frequency[idx - 1][1])
+        break;
+      if (frequency[idx][1] == frequency[idx - 1][1])
+        mode.push(frequency[idx][0]);
+    }
+  }
+
+  if (frequency.length == 1) {
+    //throw new Error('mode undefined, each num is the same');
+    return 'mode undefined, each num is the same';
+  } else if (frequency.length == mode.length) {
+    //throw new Error('mode undefined, each unique num has same frequency');
+    return 'mode undefined, each unique num has same frequency';
+  } else {
+    return mode;
+  }
+}
+
+module.exports = {
+  calcMean: calcMean,
+  calcMedian: calcMedian,
+  calcMode: calcMode,
+}
